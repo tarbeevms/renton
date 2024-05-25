09128a43e4dc3215bc1babc7b251f8d68b62f5d2
PsychicTW <135996749+PsychicTW@users.noreply.github.com>
2024-05-25 17:36:27 +0300

    Added CSS, JS and HTML for Register page

---
+++ register_page/scriptreg.js
@@ -0,0 +1,33 @@
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const phone = document.getElementById('phone').value;
+    const password = document.getElementById('password').value;
+    const account = document.getElementById('account').value;
+    const surname = document.getElementById('surname').value;
+    const name = document.getElementById('name').value;
+    
+    const data = {
+        phone: phone,
+        password: password,
+        account: account,
+        surname: surname,
+        name: name
+    };
+    
+    fetch('/api/register/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('result').style.display = 'block';
+        document.getElementById('result').textContent = JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+        console.error('Ошибка:', error);
+    });
+});
